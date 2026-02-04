8262906ba3d101ffdc8960d7603f5d1f3477a9e3
daniel <polegdaniel@gmail.com>
2026-02-04 23:26:54 +0200

    a command has risen

--- main.py
+++ main.py
@@ -1,13 +1,60 @@
 import click
+import os 
+
+
+def upper_lower_normal_generator(value):
+    for fmt in value:
+        yield fmt.lower() 
+        yield fmt.upper() 
+        yield fmt 
 
 @click.command()
-@click.argument('file', type=click.File('r'))
-class namegen:
-    """A simple CLI tool to generate names."""
-    def __init__(self, file):
-        self.file = file
+@click.argument('filename', type=click.File('r'))
+@click.option('--output', '-o', type=click.File('w'), default=None, help='Output file to save the name formats')
+def generate_name_format(filename, output):
+    """
+    generate name formats
+    """
 
-    def generate_names(self):
-        names = self.file.read().splitlines()
-        for name in names:
-            click.echo(click.style(name, fg='blue', bold=True)) 
+    try:
+        filename.open()
+    except Exception as e:
+        click.echo(f"Error opening file: {e}", err=True)
+        return
+    
+    for line in filename:
+        firstname, lastname = line.strip().split() 
+        formats = [
+            f"{firstname}.{lastname}",
+            f"{firstname}_{lastname}",
+            f"{firstname[0]}{lastname}",
+            f"{firstname}{lastname[0]}",
+            f"{lastname}.{firstname}",
+            f"{lastname}_{firstname}",
+            f"{lastname[0]}{firstname}",
+            f"{lastname}{firstname[0]}",
+        ]   
+
+        if output:
+            try:
+                for fmt in upper_lower_normal_generator(formats):
+                    output.write(fmt + '\n')
+            
+            except Exception as e:
+                click.echo(f"Error {e}", err=True)
+                for fmt in upper_lower_normal_generator(formats):
+                    click.echo(fmt)
+
+        elif output is None:
+            for fmt in upper_lower_normal_generator(formats):
+                click.echo(fmt)
+
+
+
+if __name__ == '__main__':
+    generate_name_format()
+
+
+
+
+
